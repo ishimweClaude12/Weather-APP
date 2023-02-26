12a6d0f6e5ec52c47651c88597956113f3f0c351
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-02-27 01:36:00 +0200

    Add Some javascript for requesting the api data and incorporate it on my project.

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express')
-const http = require('https')
+const https = require('node:https')
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
 
 const app = express()
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended : true}))
 
 const apiKey = "3ffa21f4913a7222de4e381db9250767"
-let day = "Sunday"
+let city = "Bali"
 app.get('/', function(req, res){
-    res.render('index.ejs', {today: day})
-    res.send()
+  //  let city= body.cityName.value;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city+ "&APPID=3ffa21f4913a7222de4e381db9250767&units=metric";
+    https.get(url, function(response){
+        let temp = ""
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            temp = weatherData.main.temp;
+        res.render('index', {temp: temp})
+     //  res.send("The temperature in London is " + temp + "degrees celcius");
+        });
+ }) 
+app.post('/', function(req, res){
+    console.log(req.body.cityName);
 })
 
-// app.get('/' ,function(req, res){
-//     res.render('index.ejs', {today: day})
-// })
+
+})
+
 app.listen(4000 , function(){
     console.log("The Server was Started at Port 4000")
-})
+});
