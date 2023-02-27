063bee5d1c1b40189894f6bdacbcc98c113943ef
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-02-27 09:09:42 +0200

    Tweak somethings around and made it accept the user input but can't pass the coresponding variable in the api search keyword

--- app.js
+++ app.js
@@ -8,25 +8,34 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended : true}))
 
 const apiKey = "3ffa21f4913a7222de4e381db9250767"
-let city = "Bali"
+let temp = "";
+let city = "";
 app.get('/', function(req, res){
   //  let city= body.cityName.value;
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city+ "&APPID=3ffa21f4913a7222de4e381db9250767&units=metric";
-    https.get(url, function(response){
-        let temp = ""
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            temp = weatherData.main.temp;
-        res.render('index', {temp: temp})
-     //  res.send("The temperature in London is " + temp + "degrees celcius");
-        });
- }) 
+  res.render('index', {temp: temp, city : city})
+
+
+
+})
 app.post('/', function(req, res){
-    console.log(req.body.cityName);
+   
+     let city =  req.body.cityName;
+     const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city+ "&APPID=3ffa21f4913a7222de4e381db9250767&units=metric";
+     
+     https.get(url, function(response){
+         
+         response.on("data", function(data){
+             const weatherData = JSON.parse(data);
+             console.log(weatherData.main.temp)
+             temp = weatherData.main.temp;
+       // res.send("The temperature in London is " + temp + "degrees celcius");
+       res.redirect('/');
+
+         });
+  }) 
+
 })
 
-
-})
 
 app.listen(4000 , function(){
     console.log("The Server was Started at Port 4000")
